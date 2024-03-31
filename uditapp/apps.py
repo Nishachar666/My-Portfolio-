f0f38aaad77fb386d6367db135b466cf0827c32a
Udit Narayan Gupta <84653087+Nishachar666@users.noreply.github.com>
2024-03-31 20:52:48 +0530

    Add files via upload

---
+++ uditapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UditappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'uditapp'
---
+++ uditapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import Home, About, Profile, Category, Skills, Portfolio, Projects
+
+
+def index(request):
+    # Home
+    home = Home.objects.latest('updated')
+
+    # About
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
+
+    # Skills
+    categories = Category.objects.all()
+
+    # Portfolio
+    portfolios = Portfolio.objects.all()
+
+    # Projects
+    projects = Projects.objects.all()
+
+    context = {
+        'home': home,
+        'about': about,
+        'profiles': profiles,
+        'categories': categories,
+        'portfolios': portfolios,
+        'projects': projects
+    }
+
+    return render(request, 'index.html', context)
